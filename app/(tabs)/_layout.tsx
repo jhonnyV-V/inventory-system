a981630d7505f526b5f3bb03bfff2149c6a8e451
JhonnyV-V <jhonny_varela_visbal@hotmail.com>
2024-09-06 18:55:53 -0400

    fix: change home tab label

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Ventas',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
           ),
